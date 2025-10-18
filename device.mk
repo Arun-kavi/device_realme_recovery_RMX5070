763510eb0b4e4b1c50b290f429edaf6d4ff4beab
Arun-kavi <arunraja327vmass@gmail.com>
2025-10-18 17:21:03 +0530

    Uncomment PRODUCT_SHIPPING_API_LEVEL in device.mk

--- device.mk
+++ device.mk
@@ -22,10 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 
 # API Level
-#PRODUCT_SHIPPING_API_LEVEL := 34
+PRODUCT_SHIPPING_API_LEVEL := 34
 # A/B
 ENABLE_VIRTUAL_AB := true
-BOARD_USES_RECOVERY_AS_BOOT := true
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
     boot \
