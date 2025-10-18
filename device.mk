005f2d016bebf5603a229b06ec575d519d043907
Arun-kavi <arunraja327vmass@gmail.com>
2025-10-18 17:44:56 +0530

    Update device.mk
    Comment out the PRODUCT_SHIPPING_API_LEVEL variable.

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 
 # API Level
-PRODUCT_SHIPPING_API_LEVEL := 34
+#PRODUCT_SHIPPING_API_LEVEL := 34
 # A/B
 ENABLE_VIRTUAL_AB := true
 AB_OTA_UPDATER := true
