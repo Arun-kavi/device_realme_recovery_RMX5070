15b4babcf49d515a0082e2ff5ce0369a5c312fa8
Arun-kavi <arunraja327vmass@gmail.com>
2025-10-19 10:58:24 +0530

    Update copyright and license information
    Updated copyright year and license identifier in the file.

--- twrp_RMX5070.mk
+++ twrp_RMX5070.mk
@@ -1,35 +1,11 @@
 #
-# Copyright 2017 The Android Open Source Project
+# Copyright (C) 2025 The Android Open Source Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# SPDX-License-Identifier: Apache-2.0
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-# The below variables will be generated automatically
-
-# Define hardware platform
-PRODUCT_RELEASE_NAME := RMX5070
-
-# Device path for OEM device tree
-DEVICE_PATH := device/$(PRODUCT_BRAND)/$(PRODUCT_DEVICE)
-
-# Inherit from hardware-specific part of the product configuration
-$(call inherit-product, $(DEVICE_PATH)/device.mk)
-
-# Inherit any OrangeFox-specific settings
-$(call inherit-product-if-exists, $(DEVICE_PATH)/fox_$(PRODUCT_RELEASE_NAME).mk)
-
-# Inherit some common TWRP stuff.
+# Inherit from TWRP common
 $(call inherit-product, vendor/twrp/config/common.mk)
-
 
 # Device identifier
 PRODUCT_DEVICE := RMX5070
@@ -37,3 +13,6 @@ PRODUCT_NAME := twrp_RMX5070
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX5070
 PRODUCT_MANUFACTURER := realme
+
+# Device path
+DEVICE_PATH := device/realme/RMX5070
